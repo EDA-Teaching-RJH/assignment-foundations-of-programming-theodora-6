8de706b91791b1a816ab3b31eee4824a19a9c3ba
theodora-6 <mjc10@kent.ac.uk>
2026-02-10 23:34:38 +0000

    fixed the type error when printing the number of high ranking officers

--- old_system.py
+++ old_system.py
@@ -63,7 +63,7 @@ def run_system_monolith():
             for rank in r:
                 if rank in ("Captain", "Commander"): 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            print("High ranking officers: ", count) 
             
         elif opt == "5":
             print("Shutting down.")
