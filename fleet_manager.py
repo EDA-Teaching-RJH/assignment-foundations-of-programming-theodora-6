436a3efba36d72b28dfbbaed1dcdf29934bdbce1
theodora-6 <mjc10@kent.ac.uk>
2026-02-13 01:16:12 +0000

    added the second function

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,3 +5,21 @@ def init_database():
     ids = ["1701", "1702", "1703", "1704", "1705"]
 
 init_database()
+
+def display_menu():
+
+        student = input("\nEnter your full name: ")
+
+        print ("\n=================================")
+        print (f"logged in as: {student}")
+        print ("starfleet crew manager")
+        print ("=================================")
+        print ("1. Add member")
+        print ("2. Remove member")
+        print ("3. Update rank")
+        print ("5. Exit")
+        print ("=================================")
+
+        choice = input ("choose an option: ")
+        return choice
+display_menu()
