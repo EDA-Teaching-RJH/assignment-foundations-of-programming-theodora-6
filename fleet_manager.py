c6c0ddd17b4436b16dd6fab0e163a7544d78785d
theodora-6 <mjc10@kent.ac.uk>
2026-02-13 01:34:57 +0000

    added function 3 fixed the menu from function 1

--- fleet_manager.py
+++ fleet_manager.py
@@ -14,12 +14,45 @@ def display_menu():
         print (f"logged in as: {student}")
         print ("starfleet crew manager")
         print ("=================================")
-        print ("1. Add member")
-        print ("2. Remove member")
-        print ("3. Update rank")
-        print ("5. Exit")
+        print("1. Add Member")
+        print("2. Remove Member")
+        print("3. Update Rank")
+        print("4. Display Roster")
+        print("5. Search Crew")
+        print("6. Filter by Division")
+        print("7. Calculate Payroll")
+        print("8. Count Senior Officers")
+        print("9. Exit")
         print ("=================================")
 
         choice = input ("choose an option: ")
         return choice
 display_menu()
+
+def add_member(names, ranks, divisions, ids):
+      
+    name = input ("enter name: ")
+    rank = input ("enter rank: ")
+    division = input ("enter divisions: ")
+
+    try:
+        crew_id = int(input("enterID:"))
+    except ValueError:
+        print("invalid ID. Must be a number.")
+
+        valid_ranks = ["captain", "commander", "lieutenant commander", "lieutenant", "ensign"]
+
+        if crew_id in ids:
+             print("error: invali rank.")
+             return
+        
+        if rank not in valid_ranks:
+             print("error: nvalid rank.")
+             return
+        
+        names.append(name)
+        ranks.append(rank)
+        divisions.append(division) 
+        ids.append(crew_id)        
+
+        print("crew member added successfully.")
